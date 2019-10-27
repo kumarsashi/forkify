944088ce8235395eb7378f1f9426e6fb04a39248
Sashi Kumar Ram <sashi.suraj7@gmail.com>
2019-10-27 10:47:20 +0530

    add clear result, limitrecipetitle

--- src/js/index.js
+++ src/js/index.js
@@ -16,20 +16,25 @@ const ControlSearch = async () => {
     
     //1. Get the query word
     const query = searchView.getInput();
-    console.log(query);
+    
 
     if(query) {
+
         //2. New search object and add it to state
         state.search = new Search(query);
-
+       
         //3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
 
         //4. Search for recpies
         await state.search.getResults();
 
         //5. Render results in UI
        // console.log(state);
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+
+       
     }
 }
 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,4 +2,50 @@ import { elements } from './base';
 
 export const getInput = () => {
    return  elements.searchInput.value;
+};
+
+export const clearInput = () => {
+   elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+   let parent = elements.resultList;
+   while(parent.firstChild) {
+        parent.firstChild.remove();
+   }
+}
+
+const limitRecipeTitle = (title, limit=20) => {
+   const newTitle = [];
+   if(title.length > limit) {
+      title.split(' ').reduce((acc, cur) => {
+         if( acc + cur.length <= limit ){
+            newTitle.push(cur);
+            
+         }
+         return acc + cur.length;
+      },0);
+      return `${newTitle.join(' ')}...`;
+   }
+   return title;
+}
+
+
+const renderRecipe = recipe => {
+   const markup = `<li>
+         <a class="results__link results__link--active" href="${recipe.recipe_id}">
+            <figure class="results__fig">
+               <img src="${recipe.image_url}" alt="Test">
+            </figure>
+            <div class="results__data">
+               <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+               <p class="results__author">${recipe.publisher}</p>
+            </div>
+         </a>
+          </li>`;
+   elements.resultList.insertAdjacentHTML('beforeend',markup);
+      
+}
+export const renderResults = recipes => {
+   recipes.forEach(renderRecipe);
 }
